6267cb945586a438914c2b2721d50ebd26c61368
Jamie Epstein <jasepstein@gmail.com>
2019-04-28 11:46:40 -0500

    HTML Route complete, API WIP

---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,10 @@
+module.exports = function (app) {
+
+   app.get("/api/friends", function (req, res) {
+       res.json(surveyData);
+   });
+
+   app.post("/api/friends", function (req, res) {
+       
+   });
+}
