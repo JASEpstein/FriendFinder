ef24770f56d7bd2413dac0141738a0d2b8428fe2
Jamie Epstein <jasepstein@gmail.com>
2019-04-28 22:24:37 -0500

    Routes functioning, no validation or logic

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -2,12 +2,11 @@ var surveyData = require("../data/friends.js")
 
 module.exports = function (app) {
 
-   app.get("/api/friends", function (req, res) {
-       res.json(surveyData);
-   });
+    app.get("/api/friends", function (req, res) {
+        res.json(surveyData);
+    });
 
-   app.post("/api/friends", function (req, res) {
-        console.log(req.body);
+    app.post("/api/friends", function (req, res) {
         surveyData.push(req.body)
-   });
+    });
 }
