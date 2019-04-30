7d4c8f5e0e145b5cc95de20f58ec543135d6648e
Jamie Epstein <jasepstein@gmail.com>
2019-04-29 21:49:58 -0500

    API routes and match logic almost complete

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -8,5 +8,17 @@ module.exports = function (app) {
 
     app.post("/api/friends", function (req, res) {
         surveyData.push(req.body)
+        var checkArray = [];
+        var reqArray = req.body.scores;
+        for (i = 0; i < surveyData.length; i++) {
+            var diff = 0;
+            for (j = 0; j < reqArray.length; j++) {
+                diff += Math.abs(parseInt(reqArray[j]) - parseInt(surveyData[i].scores[j]));
+            }
+            checkArray.push(diff);
+        }
+        var lowestDiff = Math.min.apply(null, checkArray);
+        var lowestDiffIndex = checkArray.indexOf(lowestDiff);
+        res.json(surveyData[lowestDiffIndex]);
     });
 }
